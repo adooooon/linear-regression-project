43d6b19866c1774a1c0e120ff090f33bbf134950
aden-hsu <aden.hsu@gmail.com>
2026-04-24 12:08:48 -0400

    Add files via upload

--- Project.R
+++ Project.R
@@ -1,11 +1,12 @@
 #install.packages("usethis")
 
-usethis::edit_r_environ()
+#usethis::edit_r_environ()
 Sys.getenv("CENSUS_API_KEY")
 
 #install.packages("tidycensus")
 library(tidycensus)
 library(MASS)
+library(faraway)
 
 census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
@@ -60,6 +61,8 @@ plot(df_model$pct_disability, df_model$mean_commute,
      ylab = "Mean Commute Time (minutes)",
      main = "Commute Time vs Disability Rate")
 abline(lmdisability, col = "red")
+plot(df_model$median_incomeE, df_model$mean_commute)
+
 
 
 #multivariable linear regression
@@ -109,3 +112,68 @@ hatv <- hatvalues(lrinteraction)
 head(hatv)  
 # listing large leverages:
 hatv [hatv > 2 * mean (hatv)]
+
+# find largest studentized residuals
+stud <- rstudent(lrinteraction)
+stud[which.max(abs(stud))]
+
+#Compute Bonferroni critical value: qt(1-alpha/(2n), n-p-1)
+p <- length (coefficients(lrinteraction))
+n <- nrow(model.matrix(lrinteraction))
+
+qt(1-.05/(n*2),n-p-1)
+which(abs(stud) > qt(1-.05/(n*2),n-p-1))
+# no outliers detected after bonferroni correction
+
+#cooks distance
+cooks <- cooks.distance(lrinteraction)
+h <- hatvalues(lrinteraction)
+n <- nrow(df_model)
+high_cooks <- cooks > quantile(cooks, 0.95)
+summary(high_cooks)
+
+plot(fitted(lrinteraction), df_model$mean_commute,
+     xlab = "Fitted values",
+     ylab = "Mean commute",
+     col = "blue",
+     pch = 19,
+     xlim = c(0, 65),
+     ylim = c(0, 65),
+     asp = 1,
+     xaxs = "i",
+     yaxs = "i",
+     main="Actual vs. Predicted Mean Commute Times (N=561), Adj. R-sq = 0.5152")
+abline(a = 0, b = 1, col = "black", lwd = 2, lty = 2)
+labels <- paste0(
+  "Lev=", round(h[high_cooks], 3),
+  "  D=", round(cooks[high_cooks], 3)
+)
+text(fitted(lrinteraction)[high_cooks],
+     df_model$mean_commute[high_cooks] + 0.5,  # optional vertical offset
+     labels = labels,
+     pos = 4,
+     cex = 0.7,
+     col = "red")
+high_meancommute <- names(high_cooks)[high_cooks]
+high_meancommute
+sort(cooks, decreasing = TRUE)[1:10]
+
+halfnorm(cooks, nlab = 4, ylab = "Cook's distances")
+# 0 points have a cooks distance above 0.5
+
+#confidence intervals
+confint(lrinteraction)
+summary(lrinteraction)
+
+#prediction intervals
+new_data <- data.frame(
+  median_incomeE = mean(df_model$median_incomeE),
+  total_popE = mean(df_model$total_popE),
+  pct_transit = mean(df_model$pct_transit),
+  pct_disability = mean(df_model$pct_disability),
+  median_ageE = mean(df_model$median_ageE)
+)
+predict(lrinteraction, new_data, interval = "confidence")
+predict(lrinteraction, new_data, interval = "prediction")
+summary(new_data)
+
